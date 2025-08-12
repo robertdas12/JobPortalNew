061b00ea1036ea988583058fb2c8178349e19574
robert <robert112@gmail.com>
2025-08-12 14:20:05 +0530

    first change

---
+++ src/main/java/com/Joboportal2/MainController.java
@@ -0,0 +1,15 @@
+package com.Joboportal2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class MainController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return  "welcome";
+    }
+}
